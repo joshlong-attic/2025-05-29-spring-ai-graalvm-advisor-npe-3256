ab474a180c56435d24f9bf70c8faa3dab815c496
jonghoonpark <dev@jonghoonpark.com>
2025-05-30 07:48:13 +0900

    update code for reproducing issue
    Signed-off-by: jonghoonpark <dev@jonghoonpark.com>

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,8 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.api.AdvisorChain;
 import org.springframework.ai.chat.client.advisor.api.BaseAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.beans.BeansException;
@@ -27,83 +29,20 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @Component
-    static class MyAdvisor
-            implements BaseAdvisor {
-
-
-        @Override
-        public ChatClientRequest before(ChatClientRequest chatClientRequest, AdvisorChain advisorChain) {
-            System.out.println("before " + BaseAdvisor.DEFAULT_SCHEDULER);
-            return chatClientRequest;
-        }
-
-        @Override
-        public ChatClientResponse after(ChatClientResponse chatClientResponse, AdvisorChain advisorChain) {
-            System.out.println("after " + BaseAdvisor.DEFAULT_SCHEDULER);
-
-            return chatClientResponse;
-        }
-
-        @Override
-        public int getOrder() {
-            return 0;
-        }
-    }
-
-
     @Bean
-    MessageChatMemoryAdvisor messageChatMemoryAdvisor() {
-        return MessageChatMemoryAdvisor
-                .builder(new ChatMemory() {
-                    @Override
-                    public void add(String conversationId, List<Message> messages) {
-                        System.out.println("adding " + conversationId + ":" + messages);
-                    }
-
-                    @Override
-                    public List<Message> get(String conversationId) {
-                        return List.of();
-                    }
-
-                    @Override
-                    public void clear(String conversationId) {
-                        // yup.
-                    }
-                })
+    ApplicationRunner runner(ChatClient.Builder builder) {
+        var messageChatMemoryAdvisor = MessageChatMemoryAdvisor.builder(
+                        MessageWindowChatMemory.builder()
+                                .chatMemoryRepository(new InMemoryChatMemoryRepository())
+                                .build())
+                // TODO: Should not be necessary to set this
+                // But, it fails in native image with "Caused by: java.lang.IllegalArgumentException: scheduler cannot be null"
+                // .scheduler(MessageChatMemoryAdvisor.DEFAULT_SCHEDULER)
                 .build();
-    }
 
-
-
-    // ignored
-    //    @Component
-    static class Proxy implements BeanPostProcessor {
-
-        @Override
-        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-            if (bean instanceof BaseAdvisor baseAdvisor) {
-                var proxy = new ProxyFactoryBean();
-                proxy.addAdvice((MethodInterceptor) invocation -> {
-                    System.out.println("proceeding for " + invocation.getMethod().getName() + '.');
-                    return invocation.proceed();
-                });
-                proxy.setTarget(baseAdvisor);
-                for (var c : baseAdvisor.getClass().getInterfaces())
-                    proxy.addInterface(c);
-                proxy.setProxyTargetClass(true);
-                return proxy.getObject();
-            }
-            return bean;
-        }
-    }
-
-    @Bean
-    ApplicationRunner runner(MessageChatMemoryAdvisor messageChatMemoryAdvisor, ChatClient.Builder builder,
-                             MyAdvisor advisor) {
         return _ -> {
             var ai = builder
-                    .defaultAdvisors(advisor, messageChatMemoryAdvisor)
+                    .defaultAdvisors(messageChatMemoryAdvisor)
                     .build();
             System.out.println(ai.prompt("tell me a joke").call().content());
         };
