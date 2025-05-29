034a78b94fec2c2cd577beeaa6c446b4e509999d
Josh Long <54473+joshlong@users.noreply.github.com>
2025-05-29 20:32:13 +0100

    adding build

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,24 @@
 package com.example.demo;
 
+import org.aopalliance.intercept.MethodInterceptor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.ChatClientRequest;
 import org.springframework.ai.chat.client.ChatClientResponse;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.api.AdvisorChain;
 import org.springframework.ai.chat.client.advisor.api.BaseAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -20,20 +29,20 @@ public class DemoApplication {
 
     @Component
     static class MyAdvisor
-             implements BaseAdvisor {
+            implements BaseAdvisor {
 
 
         @Override
         public ChatClientRequest before(ChatClientRequest chatClientRequest, AdvisorChain advisorChain) {
-            System.out.println("before "  + DEFAULT_SCHEDULER);
-
-            return chatClientRequest ;
+            System.out.println("before " + BaseAdvisor.DEFAULT_SCHEDULER);
+            return chatClientRequest;
         }
 
         @Override
         public ChatClientResponse after(ChatClientResponse chatClientResponse, AdvisorChain advisorChain) {
-            System.out.println("after "  + DEFAULT_SCHEDULER);
-            return chatClientResponse ;
+            System.out.println("after " + BaseAdvisor.DEFAULT_SCHEDULER);
+
+            return chatClientResponse;
         }
 
         @Override
@@ -42,11 +51,59 @@ public class DemoApplication {
         }
     }
 
+
     @Bean
-    ApplicationRunner runner(ChatClient.Builder builder, MyAdvisor advisor) {
-        return args -> {
+    MessageChatMemoryAdvisor messageChatMemoryAdvisor() {
+        return MessageChatMemoryAdvisor
+                .builder(new ChatMemory() {
+                    @Override
+                    public void add(String conversationId, List<Message> messages) {
+                        System.out.println("adding " + conversationId + ":" + messages);
+                    }
+
+                    @Override
+                    public List<Message> get(String conversationId) {
+                        return List.of();
+                    }
+
+                    @Override
+                    public void clear(String conversationId) {
+                        // yup.
+                    }
+                })
+                .build();
+    }
+
+
+
+    // ignored
+    //    @Component
+    static class Proxy implements BeanPostProcessor {
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            if (bean instanceof BaseAdvisor baseAdvisor) {
+                var proxy = new ProxyFactoryBean();
+                proxy.addAdvice((MethodInterceptor) invocation -> {
+                    System.out.println("proceeding for " + invocation.getMethod().getName() + '.');
+                    return invocation.proceed();
+                });
+                proxy.setTarget(baseAdvisor);
+                for (var c : baseAdvisor.getClass().getInterfaces())
+                    proxy.addInterface(c);
+                proxy.setProxyTargetClass(true);
+                return proxy.getObject();
+            }
+            return bean;
+        }
+    }
+
+    @Bean
+    ApplicationRunner runner(MessageChatMemoryAdvisor messageChatMemoryAdvisor, ChatClient.Builder builder,
+                             MyAdvisor advisor) {
+        return _ -> {
             var ai = builder
-                    .defaultAdvisors(advisor)
+                    .defaultAdvisors(advisor, messageChatMemoryAdvisor)
                     .build();
             System.out.println(ai.prompt("tell me a joke").call().content());
         };
